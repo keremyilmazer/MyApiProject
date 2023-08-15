6b189bddb2621ccb8500e94a7536d66789d06438
Kerem <keremyilmazerrr@hotmail.com>
2023-08-15 17:47:06 +0300

    Register Dto

---
+++ Frontend/HotelProject.WebUI/Controllers/RegisterController.cs
@@ -0,0 +1,21 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
