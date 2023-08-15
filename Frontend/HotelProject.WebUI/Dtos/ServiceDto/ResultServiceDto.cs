4537e10e312574365c3daf3c9e422b508d8ed7b7
Kerem <keremyilmazerrr@hotmail.com>
2023-08-15 15:29:25 +0300

    Hizmet sonuç Dto

--- Frontend/HotelProject.WebUI/Dtos/ServiceDto/ResultServiceDto.cs
+++ Frontend/HotelProject.WebUI/Dtos/ServiceDto/ResultServiceDto.cs
@@ -2,5 +2,9 @@
 {
     public class ResultServiceDto
     {
+        public int ServiceID { get; set; }
+        public string ServiceIcon { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
     }
 }
