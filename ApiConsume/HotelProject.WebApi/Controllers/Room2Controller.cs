bdf878f5764145d9d86d6b70558ff5cb5cea7705
Kerem <keremyilmazerrr@hotmail.com>
2023-08-15 14:49:13 +0300

    Room Mapper kontrolü

---
+++ ApiConsume/HotelProject.WebApi/Controllers/Room2Controller.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using HotelProject.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Room2Controller : ControllerBase
+    {
+        private readonly IRoomService _roomService;
+        private readonly IMapper _mapper;
+
+        public Room2Controller(IRoomService roomService, IMapper mapper)
+        {
+            _roomService = roomService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var values = _roomService.TGetList();
+            return Ok(values);
+        }
+    }
+}
