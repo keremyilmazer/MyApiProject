c9bc18270138cb7ca5cf2957941af1c7909935d5
Kerem <keremyilmazerrr@hotmail.com>
2023-08-17 15:33:31 +0300

    Booking İşlemi

--- Frontend/HotelProject.WebUI/Controllers/BookingController.cs
+++ Frontend/HotelProject.WebUI/Controllers/BookingController.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.BookingDto;
+using HotelProject.WebUI.Dtos.SubscribeDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace HotelProject.WebUI.Controllers
 {
     public class BookingController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BookingController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
         public IActionResult Index()
         {
             return View();
@@ -16,9 +26,14 @@ namespace HotelProject.WebUI.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> AddBooking(string x)
+        public async Task<IActionResult> AddBooking(CreateBookingDto createBookingDto)
         {
-            return View();
+            createBookingDto.Status = "Onay Bekliyor";
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createBookingDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            await client.PostAsync("http://localhost:5167/api/Booking", stringContent);
+            return RedirectToAction("Index", "Default");
         }
 
     }
