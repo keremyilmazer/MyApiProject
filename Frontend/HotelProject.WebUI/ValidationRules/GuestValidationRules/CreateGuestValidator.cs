d180bfa16b9f195d290c128be7dfe61922986721
Kerem <keremyilmazerrr@hotmail.com>
2023-08-23 18:50:29 +0300

    Validasyon hatasının çözümü

--- Frontend/HotelProject.WebUI/ValidationRules/GuestValidationRules/CreateGuestValidator.cs
+++ Frontend/HotelProject.WebUI/ValidationRules/GuestValidationRules/CreateGuestValidator.cs
@@ -12,7 +12,7 @@ namespace HotelProject.WebUI.ValidationRules.GuestValidationRules
             RuleFor(x => x.City).NotEmpty().WithMessage("Şehir alanı boş geçilemez");
             RuleFor(x => x.Name).MinimumLength(3).WithMessage("Lütfen en az 3 karakter veri girişi yapınız");
             RuleFor(x => x.Surname).MinimumLength(2).WithMessage("Lütfen en az 2 karakter veri girişi yapınız");
-            RuleFor(x => x.Surname).MinimumLength(3).WithMessage("Lütfen en az 3 karakter veri girişi yapınız");
+            RuleFor(x => x.City).MinimumLength(3).WithMessage("Lütfen en az 3 karakter veri girişi yapınız");
             RuleFor(x => x.Name).MaximumLength(20).WithMessage("Lütfen en fazla 3 karakter veri girişi yapınız");
             RuleFor(x => x.Surname).MaximumLength(30).WithMessage("Lütfen en fazla 2 karakter veri girişi yapınız");
             RuleFor(x => x.Surname).MaximumLength(20).WithMessage("Lütfen en fazla 3 karakter veri girişi yapınız");
