83096ccba27f79a8afabc6a5a4ddcb9e2a154400
Kerem <keremyilmazerrr@hotmail.com>
2023-08-24 17:46:34 +0300

    Gelen Mesajları Listeleme

--- Frontend/HotelProject.WebUI/Controllers/AdminContactController.cs
+++ Frontend/HotelProject.WebUI/Controllers/AdminContactController.cs
@@ -1,12 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.ContactDto;
+using HotelProject.WebUI.Models.Staff;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HotelProject.WebUI.Controllers
 {
     public class AdminContactController : Controller
     {
-        public IActionResult Inbox()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminContactController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Inbox()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5167/api/Contact");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<InboxContactDto>>(jsonData);
+                return View(values);
+            }
             return View();
+        }
+        public PartialViewResult SideBarAdminContactPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult SideBarAdminContactCategoryPartial()
+        {
+            return PartialView();
         }
     }
 }
