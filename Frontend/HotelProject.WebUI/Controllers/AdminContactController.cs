94af577bc7a5c693254ff5acd9dcc272a6cbf506
Kerem <keremyilmazerrr@hotmail.com>
2023-08-25 16:47:45 +0300

    Mesaj İşlemlerinin Tamamlanması

--- Frontend/HotelProject.WebUI/Controllers/AdminContactController.cs
+++ Frontend/HotelProject.WebUI/Controllers/AdminContactController.cs
@@ -71,7 +71,7 @@ namespace HotelProject.WebUI.Controllers
         {
             return PartialView();
         }
-        public async Task<IActionResult> MessageDetails(int id)
+        public async Task<IActionResult> MessageDetailsBySendbox(int id)
         {
             var client = _httpClientFactory.CreateClient();
             var responseMessage = await client.GetAsync($"http://localhost:5167/api/SendMessage/{id}");
@@ -79,6 +79,19 @@ namespace HotelProject.WebUI.Controllers
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<GetMessageByIDDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> MessageDetailsByInbox(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"http://localhost:5167/api/Contact/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<InboxContactDto>(jsonData);
                 return View(values);
             }
             return View();
